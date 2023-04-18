6754902fe6d4a3bd96dc35ae71f9a8dbf83887b2
THIAGO DO NASCIMENTO DA COSTA <04395525326@icc.local>
2023-04-18 19:22:10 -0300

    define models

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/chatservice
 
 go 1.20
+
+require github.com/google/uuid v1.3.0
+
+require (
+	github.com/j178/tiktoken-go v0.2.1 // indirect
+	github.com/sashabaranov/go-openai v1.5.8 // indirect
+)
--- internal/domain/entity/chat.go
+++ internal/domain/entity/chat.go
@@ -1,13 +1,36 @@
 package entity
 
+import "errors"
+
 type ChatConfig struct {
-	Model *Model
+	Model            *Model
+	Temperature      float32
+	TopP             float32
+	N                int
+	Stop             []string
+	MaxTokens        int
+	PresencePenalty  float32
+	FrequencyPenalty float32
 }
 
 type Chat struct {
-	ID         string
-	UserID     string
-	Status     string
-	TokenUsage int
-	Config     *ChatConfig
+	ID                   string
+	UserID               string
+	InitialSystemMessage *Message
+	Messages             []*Message
+	ErasedMesages        []*Message
+	Status               string
+	TokenUsage           int
+	Config               *ChatConfig
+}
+
+func (c *Chat) AddMessage(m *Message) error {
+	if c.Status == "ended" {
+		return errors.New("chat is already ended, no more messages allowed")
+	}
+	for {
+		if c.Config.Model.GetMaxToken() >= m.GetQtdTokens()+c.TokenUsage {
+			c.Messages = append(c.Messages, m)
+		}
+	}
 }
